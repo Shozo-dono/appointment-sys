3edd60d5d57fa8420ebe58ccc3bca9c3464defba
Shamir Rasul <rasulshamir@gmail.com>
2026-06-01 13:29:43 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1 class="text-3xl font-bold underline bg-red-500">Hello world! roms</h1>
+  );
+}
+
+export default App;
